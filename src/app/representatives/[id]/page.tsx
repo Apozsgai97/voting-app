54ae6f2eb90f788977439eb0f531fcee3f2228af
Adrienn Pozsgai <apozsgai97@gmail.com>
2024-11-22 14:03:23 +0100

    add basic html to representative by id route

--- src/app/representatives/[id]/page.tsx
+++ src/app/representatives/[id]/page.tsx
@@ -1,6 +1,21 @@
 export default async function Page(props: { params: Promise<{ id: string }> }) {
   const params = await props.params;
   const id = params.id;
+  console.log({id})
 
-  return <h1>{id}</h1>;
+  return (
+    <main>
+      <h1>Representative name</h1>
+      <button>Vote</button>
+      <button>Unvote</button>
+      <p>Public votes</p>
+      <div>
+       <h2>Previous votes</h2>
+       <div>
+         <p>choice</p>
+         <p>agreement rate</p>
+       </div>
+      </div>
+    </main>
+  );
 }
