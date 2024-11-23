c511c1cd4d646012f66cd0e4f6919f0cb55527b9
Adrienn Pozsgai <apozsgai97@gmail.com>
2024-11-23 11:57:01 +0100

    change styling for representative by id page

--- src/app/representatives/[id]/page.tsx
+++ src/app/representatives/[id]/page.tsx
@@ -4,17 +4,52 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
   console.log({id})
 
   return (
-    <main>
-      <h1>Representative name</h1>
-      <button>Vote</button>
-      <button>Unvote</button>
-      <p>Public votes</p>
+    <main className="flex flex-col justify-center items-center p-0">
+      <h1 className="text-center text-4xl font-bold my-10 mt-28">
+        Representative name
+      </h1>
+      <div className="flex w-48 items-center justify-between">
+        <button
+          type="submit"
+          className="btn bg-emerald-900 text-gray-100 hover:bg-emerald-800 rounded-lg w-20"
+        >
+          Vote
+        </button>
+        <button
+          type="submit"
+          className="btn bg-emerald-900 text-gray-100 hover:bg-emerald-800 rounded-lg w-20"
+        >
+          Unvote
+        </button>
+      </div>
+      <article className="stats shadow my-8">
+        <div className="stat text-center">
+          <div className="stat-title">Public votes</div>
+          <div className="stat-value">0</div>
+        </div>
+      </article>
       <div>
-       <h2>Previous votes</h2>
-       <div>
-         <p>choice</p>
-         <p>agreement rate</p>
-       </div>
+        <h2 className="text-2xl font-bold">Previous votes</h2>
+        <div className="overflow-x-auto">
+          <table className="table">
+            <thead>
+              <tr>
+                <th></th>
+                <th>Issue</th>
+                <th>Choice</th>
+                <th>Agreement Rate</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th>1</th>
+                <td>Cat or Dog</td>
+                <td>Cat</td>
+                <td>60%</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </main>
   );
