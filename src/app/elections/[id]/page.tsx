2b7345ee9e19b4e8150f6164c3d7c62f5a31d596
Adrienn Pozsgai <apozsgai97@gmail.com>
2024-11-24 11:17:32 +0100

    separate ongoing and closed elections

--- src/app/elections/[id]/page.tsx
+++ src/app/elections/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { ClosedElection } from "@/features/elections/components/closed-election";
 import { OngoingElection } from "@/features/elections/components/ongoing-election";
 import { electionFeature } from "@/features/elections/instance";
+import { notFound } from "next/navigation";
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
   const params = await props.params;
@@ -8,10 +9,18 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
   
   const election = await electionFeature.service.getElectionById(id)
 
+   if (!election) {
+     notFound();
+   }
+
+
   return (
     <main className="flex flex-col justify-center items-center p-0">
-     <OngoingElection election={election!}/>
-     <ClosedElection election={election!}/>
+      {election.status === "ongoing" ? (
+        <OngoingElection election={election} />
+      ) : (
+        <ClosedElection election={election} />
+      )}
     </main>
   );
 }
