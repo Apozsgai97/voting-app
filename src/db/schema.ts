addc41bc2935669ab5ebd73f91bccde3b5f6de7e
Adrienn Pozsgai <apozsgai97@gmail.com>
2024-12-01 08:40:00 +0100

    add representative and election vote tables

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,12 +1,5 @@
 import { integer, pgTable, text, uuid, varchar } from "drizzle-orm/pg-core";
 
-export const usersTable = pgTable("users", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  name: varchar({ length: 255 }).notNull(),
-  age: integer().notNull(),
-  email: varchar({ length: 255 }).notNull().unique(),
-});
-
 export const electionsTable = pgTable("elections", {
   id: uuid("id").primaryKey().defaultRandom(),
   issue: text("issue").notNull(),
@@ -18,4 +11,24 @@ export const electionsTable = pgTable("elections", {
   choice2_result: varchar("choice2_result", { length: 255 }).notNull(),
   status: varchar("status", { length: 255 }).notNull(),
   publish_date: varchar("publish_date", { length: 255 }).notNull(),
+});
+
+export const representativesTable = pgTable("representatives", {
+  id: uuid("id").primaryKey().notNull().defaultRandom(),
+  name: varchar("name", { length: 255 }).notNull(),
+  email: varchar("email", { length: 255 }).notNull().unique(),
+  publicVotes: integer("public_votes").default(0).notNull(),
+});
+
+export const electionVotesTable = pgTable("election votes", {
+  id: uuid("id").primaryKey().notNull().defaultRandom(),
+  representativeId: uuid("representative_id")
+    .notNull()
+    .references(() => representativesTable.id),
+  electionId: uuid("election_id").notNull(),
+  choice1Votes: integer("choice_1_votes").notNull(),
+  choice2Votes: integer("choice_2_votes").notNull(),
+  currentPublicVotes: integer("current_public_votes").notNull(),
+  choice: varchar("choice", { length: 255 }).notNull(),
+  agreementRate: integer("agreement_rate").notNull(),
 });
