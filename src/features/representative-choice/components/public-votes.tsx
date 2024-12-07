e56414109d412a5bf142db207713368865c603c0
Adrienn Pozsgai <apozsgai97@gmail.com>
2024-12-07 17:43:13 +0100

    add representative email to public votes

--- src/features/representative-choice/components/public-votes.tsx
+++ src/features/representative-choice/components/public-votes.tsx
@@ -6,11 +6,17 @@ export function PublicVotes({
   representative: Representative;
 }) {
   return (
-    <article className="stats shadow my-8">
-      <div className="stat text-center">
-        <div className="stat-title">Public votes</div>
-        <div className="stat-value">{representative?.publicVotes || 0}</div>
-      </div>
-    </article>
+    <>
+      <article className="stats shadow my-8">
+        <div className="stat text-center">
+          <div className="stat-title">Public votes</div>
+          <div className="stat-value">{representative?.publicVotes || 0}</div>
+        </div>
+      </article>
+      <p>
+        <span className="font-bold">Email:</span>{" "}
+        {representative?.email || "example@gmail.com"}
+      </p>
+    </>
   );
 }
